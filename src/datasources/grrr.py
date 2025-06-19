69f30ebb773af584cab19b5be4b51ea95550b34c
hannahker <hannah.ker96@gmail.com>
2025-06-19 10:57:06 -0700

    add source code for processing google data

---
+++ src/datasources/grrr.py
@@ -0,0 +1,49 @@
+import os
+
+import xarray as xr
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+BASE_DIRECTORY = (
+    "gs://flood-forecasting/hydrologic_predictions/model_id_8583a5c2_v0/"
+)
+
+
+def open_zarr(path):
+    return xr.open_zarr(
+        store=path, chunks="auto", storage_options=dict(token="anon")
+    )
+
+
+def process_reforecast(gauge):
+    reforecast_path = os.path.join(
+        BASE_DIRECTORY, "reforecast/streamflow.zarr/"
+    )
+    ds_rf = open_zarr(reforecast_path).sel(gauge_id=gauge).compute()
+    df_rf = ds_rf.to_dataframe().reset_index()
+    df_rf["valid_time"] = df_rf.apply(
+        lambda row: row["issue_time"] + row["lead_time"], axis=1
+    )
+    df_rf["leadtime"] = df_rf["lead_time"].apply(lambda x: x.days)
+    df_rf = df_rf.drop(columns=["lead_time", "gauge_id"])
+    return df_rf
+
+
+def process_reanalysis(gauge):
+    reanalysis_path = os.path.join(
+        BASE_DIRECTORY, "reanalysis/streamflow.zarr/"
+    )
+    ds_ra = open_zarr(reanalysis_path).sel(gauge_id=gauge).compute()
+    df_ra = ds_ra.to_dataframe().reset_index()
+    df_ra = df_ra.rename(columns={"time": "valid_time"}).drop(
+        columns=["gauge_id"]
+    )
+    return df_ra
+
+
+def load_return_periods(gauge):
+    return_periods_path = os.path.join(BASE_DIRECTORY, "return_periods.zarr/")
+    return open_zarr(return_periods_path).sel(gauge_id=gauge).compute()
